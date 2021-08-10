cb2dc4e8aac5a94862c8caaa34b2510fc7b5bac5
Lyubovir <lubovirs@gmail.com>
2021-08-10 13:45:40 +0300

    Initial upload of SeoTools

---
+++ Data/SeoTools.Data.Common/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace SeoTools.Data.Common.Repositories
+{
+    using System.Linq;
+
+    using SeoTools.Data.Common.Models;
+
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
---
+++ Web/SeoTools.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿namespace SeoTools.Web.Areas.Administration.Controllers
+{
+    using SeoTools.Services.Data;
+    using SeoTools.Web.ViewModels.Administration.Dashboard;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DashboardController : AdministrationController
+    {
+        private readonly ISettingsService settingsService;
+
+        public DashboardController(ISettingsService settingsService)
+        {
+            this.settingsService = settingsService;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = new IndexViewModel { SettingsCount = this.settingsService.GetCount(), };
+            return this.View(viewModel);
+        }
+    }
+}
